09b1aeef3a78597eb20c16decbcc6c7d3d413960
kkkkk1109 <kao891109@gmail.com>
2024-02-29 07:37:09 -0800

    Implement queue function
    the following functions are  implemented
    q_delete_mid
    q_delete_dup
    q_reverse

--- queue.c
+++ queue.c
@@ -69,13 +69,31 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head)) {
+        return NULL;
+    }
+    element_t *rv_node = list_first_entry(head, element_t, list);
+    list_del(&rv_node->list);
+    if (sp) {
+        memcpy(sp, rv_node->value, bufsize);
+        sp[bufsize - 1] = '\0';
+    }
+    return rv_node;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head)) {
+        return NULL;
+    }
+    element_t *rv_node = list_last_entry(head, element_t, list);
+    list_del(&rv_node->list);
+    if (sp) {
+        memcpy(sp, rv_node->value, bufsize);
+        sp[bufsize - 1] = '\0';
+    }
+    return rv_node;
 }
 
 /* Return number of elements in queue */
@@ -96,12 +114,41 @@ int q_size(struct list_head *head)
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head))
+        return false;
+    struct list_head *ln, *rn;
+    ln = head->prev;
+    rn = head->next;
+    while (1) {
+        if ((ln == rn) | (rn->next == ln))
+            break;
+        ln = ln->prev;
+        rn = rn->next;
+    }
+    element_t *rv_node = list_entry(rn, element_t, list);
+    list_del(rn);
+    q_release_element(rv_node);
     return true;
 }
 
 /* Delete all nodes that have duplicate string */
 bool q_delete_dup(struct list_head *head)
 {
+    if (!head || list_empty(head) || list_is_singular(head))
+        return false;
+    element_t *cur, *nxt;
+    bool dup = false;
+    list_for_each_entry_safe (cur, nxt, head, list) {
+        if (strcmp(cur->value, nxt->value) == 0 && &nxt->list != head) {
+            list_del(&cur->list);
+            q_release_element(cur);
+            dup = true;
+        } else if (dup) {
+            list_del(&cur->list);
+            q_release_element(cur);
+            dup = false;
+        }
+    }
     // https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
     return true;
 }
@@ -113,7 +160,19 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head))
+        return;
+
+    struct list_head *node, *safe;
+    list_for_each_safe (node, safe, head) {
+        node->next = node->prev;
+        node->prev = safe;
+    }
+    node->next = node->prev;
+    node->prev = safe;
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
