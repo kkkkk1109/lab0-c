5ef5ce708eb746ea362e2f6bfa90e4019834a192
kkkkk1109 <kao891109@gmail.com>
2024-03-01 01:21:07 -0800

    Rewrite q_insert_head
    use q_element_release instead of free.

--- queue.c
+++ queue.c
@@ -44,7 +44,7 @@ bool q_insert_head(struct list_head *head, char *s)
         return false;
     new_ele->value = strdup(s);
     if (!new_ele->value) {
-        free(new_ele);
+        q_release_element(new_ele);
         return false;
     }
     list_add(&new_ele->list, head);
